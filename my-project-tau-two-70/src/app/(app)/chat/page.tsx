1f671ea79c33f00843bdce39934904136b32cd39
Z User <z@container>
2026-04-26 03:48:07 +0000

    chore: remove redundant my-project-tau-two-70 directory

--- my-project-tau-two-70/src/app/(app)/chat/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ChatView } from "@/components/dashboard/chat-view";
-
-export default function ChatPage() {
-  return (
-    <div className="h-[calc(100dvh-60px)] lg:h-screen flex flex-col">
-      <ChatView />
-    </div>
-  );
-}
